e295336075923342bad0149b1ac19b4c151ec5f3
johnmendozai5 <jmendoz223@gmail.com>
2026-06-22 22:50:56 -0500

    income-tax Step 3: federal tax brackets by status with round

--- income-tax/main.cpp
+++ income-tax/main.cpp
@@ -18,7 +18,7 @@ int main(){
 
     if(gross_income > 120000){
         std::cout << "Error: Income too high to use this form" << std::endl;
-    }else{
+    } else{
         if(status != 2){ status = 1; }
     // output deduction and taxable income
     if(status == 1){
@@ -38,6 +38,32 @@ int main(){
     std::cout << "Deduction: $" << deduction << std::endl;
     // print taxable income
     std::cout << "Taxable income: $" << taxable_income << std::endl;
+
+    double tax;
+
+    if(status == 1){
+        if(taxable_income<= 10000){
+            tax = taxable_income * 0.10;
+        } else if(taxable_income <= 40000){
+            tax = 1000 + (taxable_income - 10000) * 0.12;
+        } else if(taxable_income <= 85000){
+            tax = 4600 + (taxable_income - 40000) * 0.22;
+        } else {
+            tax = 14500 + (taxable_income - 85000) * 0.24;
+        }
+    } else {
+        if(taxable_income <= 20000){
+            tax = taxable_income * 0.10;
+        } else if(taxable_income <= 80000){
+            tax = 2000 + (taxable_income - 20000) * 0.12;
+        } else {
+            tax = 9200 + (taxable_income - 80000) * 0.22;
+        }
+
+}
+        
+    tax = round(tax);
+    std::cout << "Federal tax: $" << tax << std::endl;
 }
 
 }
