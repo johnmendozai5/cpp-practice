12fd987e77dd3a7f9be38ed547a5832006eec26e
john m <johnm@Johnnys-MBP.attlocal.net>
2026-06-25 17:36:41 -0500

    toll-roads: working CalcToll using isMorning and isWeekend

--- toll-roads/main.cpp
+++ toll-roads/main.cpp
@@ -2,17 +2,31 @@
 #include <iomanip>
 
 
-double CalcToll(int hour, bool isMorining, bool isWeekend) {
-
+double CalcToll(int hour, bool isMorning, bool isWeekend) {
+	if(isWeekend) {
+		if(isMorning) {
+			return 6.05;
+		} else {
+			return 7.15;
+		}
+	} else {
+		if (isMorning) {
+			return 7.95;
+		} else {
+			return 6.90;
+		}
+	}
 }
 
 int main() {
 
+	std::cout << std::fixed << std::setprecision(2);
 
-	// cout << CalcToll(8, true, false) << endl;   // 7.95
-	// cout << CalcToll(1, false, false) << endl;  // 6.9
-	// cout << CalcToll(3, false, true) << endl;   // 7.15
-	// cout << CalcToll(5, true, true);            // 6.05
+
+	std::cout << CalcToll(8, true, false) << std::endl;   // 7.95
+	std::cout << CalcToll(1, false, false) << std::endl;  // 6.9
+	std::cout << CalcToll(3, false, true) << std::endl;   // 7.15
+	std::cout << CalcToll(5, true, true);            // 6.05
 
 	return 0;
 }
