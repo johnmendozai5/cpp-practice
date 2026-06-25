e235f1a56e138d53432f860b52f1fc1a42d0ea14
johnmendozai5 <jmendoz223@gmail.com>
2026-06-25 13:08:45 -0500

    toll-roads: CalcToll function shell

---
+++ toll-roads/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+
+double CalcToll(int hour, bool isMorining, bool isWeekend) {
+
+}
+
+int main() {
+
+
+	// cout << CalcToll(8, true, false) << endl;   // 7.95
+	// cout << CalcToll(1, false, false) << endl;  // 6.9
+	// cout << CalcToll(3, false, true) << endl;   // 7.15
+	// cout << CalcToll(5, true, true);            // 6.05
+
+	return 0;
+}
